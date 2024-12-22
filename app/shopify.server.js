ff8d013318e3f3f5a3b8dcf4bab90fa733acc970
Long Pham <longphp@192.168.1.12>
2024-12-23 00:57:46 +0700

    init commit

---
+++ app/shopify.server.js
@@ -0,0 +1,51 @@
+import "@shopify/shopify-app-remix/adapters/node";
+import {
+  ApiVersion,
+  AppDistribution,
+  shopifyApp,
+} from "@shopify/shopify-app-remix/server";
+import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prisma";
+import prisma from "./db.server";
+import installAppQueue from './queue'; // Import the queue
+
+const shopify = shopifyApp({
+  apiKey: process.env.SHOPIFY_API_KEY,
+  apiSecretKey: process.env.SHOPIFY_API_SECRET || "",
+  apiVersion: ApiVersion.October24,
+  scopes: process.env.SCOPES?.split(","),
+  appUrl: process.env.SHOPIFY_APP_URL || "",
+  authPathPrefix: "/auth",
+  sessionStorage: new PrismaSessionStorage(prisma),
+  distribution: AppDistribution.AppStore,
+  isEmbeddedApp: true,
+  hooks: {
+    afterAuth: async ({ session, admin }) => {
+      console.log('Authentication successful for shop:', session.shop);
+      
+      shopify.registerWebhooks({ session })
+
+      await installAppQueue.add({
+        shop: session.shop,
+        accessToken: session.accessToken,
+      });
+
+      // await Promise.all([initShopData(admin, session), installApp(prisma, admin.graphql, session)])
+    },
+  },
+  future: {
+    unstable_newEmbeddedAuthStrategy: true,
+    removeRest: true,
+  },
+  ...(process.env.SHOP_CUSTOM_DOMAIN
+    ? { customShopDomains: [process.env.SHOP_CUSTOM_DOMAIN] }
+    : {}),
+});
+
+export default shopify;
+export const apiVersion = ApiVersion.October24;
+export const addDocumentResponseHeaders = shopify.addDocumentResponseHeaders;
+export const authenticate = shopify.authenticate;
+export const unauthenticated = shopify.unauthenticated;
+export const login = shopify.login;
+export const registerWebhooks = shopify.registerWebhooks;
+export const sessionStorage = shopify.sessionStorage;
