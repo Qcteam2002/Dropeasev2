01f4c801833ae0b786d79d786fb366f917c2e4f4
Long Pham <longphp@192.168.1.2>
2025-03-17 07:12:02 +0700

    refactor schema

--- app/models/PlatformProduct.js
+++ app/models/PlatformProduct.js
@@ -2,6 +2,17 @@ import qrcode from "qrcode";
 import invariant from "tiny-invariant";
 import db from "../db.server";
 
+export async function getProducts(user) {
+  const products = await db.platformProduct.findMany({
+    where: { user },
+    orderBy: { id: "desc" },
+  });
+
+  if (products.length === 0) return [];
+
+  return products;
+}
+
 export async function getProduct(id) {}
 
 export async function upsert(product,session) {
@@ -24,35 +35,3 @@ export async function upsert(product,session) {
 
 }
 
-export async function getProducts(shop, graphql) {
-  const qrCodes = await db.qRCode.findMany({
-    where: { shop },
-    orderBy: { id: "desc" },
-  });
-
-  if (qrCodes.length === 0) return [];
-
-  return Promise.all(
-    qrCodes.map((qrCode) => supplementQRCode(qrCode, graphql))
-  );
-}
-
-export function validateQRCode(data) {
-  const errors = {};
-
-  if (!data.title) {
-    errors.title = "Title is required";
-  }
-
-  if (!data.productId) {
-    errors.productId = "Product is required";
-  }
-
-  if (!data.destination) {
-    errors.destination = "Destination is required";
-  }
-
-  if (Object.keys(errors).length) {
-    return errors;
-  }
-}
